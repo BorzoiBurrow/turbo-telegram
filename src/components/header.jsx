91f00728f06bf25b797158022c201739388df4ae
Brad <Bradrockk@gmail.com>
2024-03-21 05:16:41 -0400

    added some logic to navbar, some minor styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,16 +2,34 @@ import React from "react";
 
 
 const Header = () => {
+    // handles button presses for navbar, logic still needs to made
+    const handleClick = (buttonName) => {
+        const actions = {
+            "Home": () => console.log("Heading home..."),
+            "bored?": () => console.log("Me too..."),
+            "blog": () => console.log("Feeling chatty."),
+            "default": () => {
+                console.log("button name was invalid, verify that the button is passing the correct name, or that the object case supports the button.");
+                console.log(`The button name that was pressed is ${buttonName}`);
+            }}; 
+    (actions[buttonName] || actions["default"])(); 
+}; 
     return (
         <menu className="navbar">
             <li className="navbar-item">
-                <div className="navbar-card">Home</div>
+                <button onClick={() => handleClick("Home")} className="navbar-card">
+                <div>Home</div>
+                </button>
             </li>
             <li className="navbar-item">
-                <div className="navbar-card">bored?</div>
-            </li>
-            <li className="navbar-item">
-                <div className="navbar-card">blog</div>
+                <button onClick={() => handleClick("bored?")} className="navbar-card">
+                <div>bored?</div>
+                </button>
+            </li>           
+             <li className="navbar-item">
+                <button onClick={() => handleClick("blog")} className="navbar-card">
+                <div>blog</div>
+                </button>
             </li>
         </menu>
     )
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client';
 import Header from "./components/header";
 import "../src/assets/css/index.css";
 
-// testing message for dev 
-console.log("hello!")
-
 // await DOM load before executing react elements.
 window.addEventListener("DOMContentLoaded", function () {
     const headerRoot = createRoot(document.getElementById('headerRoot')).render(<Header/>);
