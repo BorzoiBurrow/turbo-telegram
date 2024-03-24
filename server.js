742d1af9aee3f19ebf33a8f3d3ef4624fe107b48
Brad <Bradrockk@gmail.com>
2024-03-24 10:42:48 -0400

    adjusted page routing for new pages

--- server.js
+++ server.js
@@ -8,14 +8,25 @@ let port = process.env.port || 3001;
 const serve = path.join(__dirname, "/dist")
 process.chdir(serve)
 
-// home page routing.
+// page routing.
 app.get("/", (req,res) => {
     res.sendFile(serve + "/assets/pages/index.html")
 });
 
+
+app.get("/blog", (req,res) => {
+    res.sendFile(serve + "/assets/pages/blog.html")
+})
+
+app.get("/bored", (req,res) =>{
+    res.sendFile(serve + "/assets/pages/bored.html")
+})
+
+// retrieve webpack bundle
 app.get("/bundle.js", (req,res) => {
     res.sendFile(serve + "/bundle.js")
 })
+
 // initalize the app.
 app.listen(port, () => {
 console.log(`View the app at http://localhost:${port}`)
