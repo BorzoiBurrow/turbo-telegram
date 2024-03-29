b3e944d10b5a18d96e02e225fcc89abeb2e93452
Brad <Bradrockk@gmail.com>
2024-03-29 13:36:09 -0400

    some work towards verifiying creation of accounts

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 const express = require("express");
 const path = require('path')
 const app = express();
-const verify = require("./src/assets/scripts/verify")
+const mysql = require('mysql2/promise');
+require('dotenv').config()
+
+
 
 // set working path and app port.
 let port = process.env.port || 3001;
@@ -31,6 +34,10 @@ app.get("/Create", (req,res) => {
     res.sendFile(serve + "/assets/CreateAccount.html")
 })
 
+app.post("/Create", (req,res) => {
+
+})
+
 // retrieve webpack bundle
 app.get("/bundle.js", (req,res) => {
     res.sendFile(serve + "/bundle.js")
@@ -41,3 +48,38 @@ app.listen(port, () => {
 console.log(`View the app at http://localhost:${port}`)
 console.log(`listening live on ${port}!`)
 });
+
+
+async function VerifyAccount(Email){
+    // check if the email and name is already in the database. Will need to recieve email from the user to check. 
+    try {
+
+        const connection = await mysql.createConnection({
+            host: process.env.host,
+            user: process.env.user,
+            database: process.env.database,
+            password: process.env.password
+          });
+// placeholder for now. Should be the email of the user.
+        
+        const [results] = await connection.query(
+            `SELECT * FROM ACCOUNTS WHERE Email = ?`, [Email]
+        );
+        if (results.length === 0) {
+            console.log("Email not found, continue....");
+        } else {
+            console.log("Email was previously Used.")
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function CreateAccount(){
+    // take in the data from the user then create the account in the database. 
+
+    // Provide a session for ten minutes.
+
+    // reroute them to the main page.
+}
+
