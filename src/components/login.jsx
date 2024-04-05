fe6314d7804fdac96046aa4518f472f5ee5942f3
Brad <Bradrockk@gmail.com>
2024-04-04 22:24:22 -0400

    inital login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login =  () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(userName, password)
+        fetch('/login', {
 
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({userName, password})
+        })
+    }
     return (
-        <h1>New to the site? Sign up <a href="/Create">Here.</a> </h1>
-    )
-}
+        <form id="CreationForm">
+        <label className="InputLabel" htmlFor="UserName">Please your Username.</label>
+        <input className="InputField" id="UserName" type="text" autoComplete="username" onChange={(e) => setUserName(e.target.value)}/>
+    
+        <label className="InputLabel" htmlFor="Password">Please enter your password. </label>
+        <input className="InputField" id="Password" type="text" autoComplete="Password" onChange={(e) => setPassword(e.target.value)}/>
+
+        <button id="submit" onClick={handleSubmit} >Submit</button>
+        </form>
+    )}
+
 
 export default Login
